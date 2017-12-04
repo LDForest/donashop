5e0e0765d2a7e3258133405ec550920e066cb121
Kartushin Aleksei <akartushin@yamoney.ru>
2017-12-04 20:06:38 +0300

    Events image

--- data/mock/indexEventsSlides.js
+++ data/mock/indexEventsSlides.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     type: 'event',
     description: 'London Fashion Week September 2017',
+    image: '/images/event-slides/slide-1.jpg',
     date: '15th - 19th September 2017',
   },
 ];
